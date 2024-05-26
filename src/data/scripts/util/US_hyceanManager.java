870d477657626215900ae85c77de8d3d72462792
JamesTripleQ <james05q@gmail.com>
2024-05-26 22:55:58 +0200

    Hycean part 2.1

--- src/data/scripts/util/US_hyceanManager.java
+++ src/data/scripts/util/US_hyceanManager.java
@@ -1,16 +1,11 @@
 package data.scripts.util;
 
-import com.fs.graphics.H;
-import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.PlanetAPI;
 import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Conditions;
 import com.fs.starfarer.api.impl.campaign.procgen.ConditionGenDataSpec;
-import com.fs.starfarer.api.impl.campaign.procgen.PlanetGenDataSpec;
 import com.fs.starfarer.api.impl.campaign.procgen.StarSystemGenerator;
-import com.fs.starfarer.api.util.Pair;
 import com.fs.starfarer.api.util.WeightedRandomPicker;
-import com.fs.starfarer.campaign.econ.MarketCondition;
 
 import java.util.*;
 
@@ -19,32 +14,74 @@ import static com.fs.starfarer.api.impl.campaign.procgen.PlanetConditionGenerato
 import static data.scripts.util.US_utils.addConditionIfNeeded;
 
 public class US_hyceanManager {
-    private static final Map<String, Float> baseWeight = new HashMap<>();
+    public static Map<String, Float> baseWeight = new HashMap<>();
+    private static final List<String> groups = new ArrayList<>();
 
     static {
+        groups.add("volatiles");
+        groups.add("organics");
+
+        /*
+        baseWeight.put("atmosphere_no_pick", 0f);
+        baseWeight.put(Conditions.DENSE_ATMOSPHERE, 0f);
+
+        baseWeight.put("weather_no_pick", 0f);
+        baseWeight.put(Conditions.EXTREME_WEATHER, 0f);
+        */
+
+        /*
+        pollution_no_pick
+        pollution
+
+        ore_no_pick
+        ore_sparse
+        ore_moderate
+        ore_abundant
+        ore_rich
+        ore_ultrarich
+
+        rare_ore_no_pick
+        rare_ore_sparse
+        rare_ore_moderate
+        rare_ore_abundant
+        rare_ore_rich
+        rare_ore_ultrarich
+        */
+
+        // Weights are WIP
+        baseWeight.put("organics_no_pick", 20f);
         baseWeight.put(Conditions.ORGANICS_TRACE, 0f);
         baseWeight.put(Conditions.ORGANICS_COMMON, 5f);
         baseWeight.put(Conditions.ORGANICS_ABUNDANT, 20f);
         baseWeight.put(Conditions.ORGANICS_PLENTIFUL, 10f);
 
+        baseWeight.put("volatiles_no_pick", 20f);
         baseWeight.put(Conditions.VOLATILES_TRACE, 0f);
         baseWeight.put(Conditions.VOLATILES_DIFFUSE, 5f);
         baseWeight.put(Conditions.VOLATILES_ABUNDANT, 20f);
         baseWeight.put(Conditions.VOLATILES_PLENTIFUL, 10f);
+
+        /*
+        US_special_no_pick
+        US_religious
+         */
     }
 
 
     public static void manageHyceanConditions(PlanetAPI planet) {
         addConditionIfNeeded(planet, Conditions.WATER_SURFACE);
 
-        WeightedRandomPicker<String> organics = getGroupPicker(planet, "organics");
-        WeightedRandomPicker<String> volatiles = getGroupPicker(planet, "volatiles");
+        for (String group : groups) {
+            WeightedRandomPicker<String> picker = getGroupPicker(planet, group);
+            String condition = picker.pick();
 
-        addConditionIfNeeded(planet, organics.pick());
-        addConditionIfNeeded(planet, volatiles.pick());
+            if (!condition.endsWith(ConditionGenDataSpec.NO_PICK_SUFFIX)) {
+                addConditionIfNeeded(planet, condition);
+            }
+        }
     }
 
-    public static WeightedRandomPicker<String> getGroupPicker(PlanetAPI planet, String group) {
+    private static WeightedRandomPicker<String> getGroupPicker(PlanetAPI planet, String group) {
 
         Set<String> conditionsSoFar = new HashSet<>();
 
