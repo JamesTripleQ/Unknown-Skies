562b82f02cda655a7c5a67216fb987a95873aad2
JamesTripleQ <james05q@gmail.com>
2024-05-28 22:31:58 +0200

    Hycean part 2.5

--- src/data/scripts/util/US_hyceanManager.java
+++ src/data/scripts/util/US_hyceanManager.java
@@ -12,13 +12,57 @@ import java.util.*;
 import static com.fs.starfarer.api.impl.campaign.procgen.PlanetConditionGenerator.getDataForGroup;
 import static com.fs.starfarer.api.impl.campaign.procgen.PlanetConditionGenerator.preconditionsMet;
 import static data.scripts.util.US_utils.addConditionIfNeeded;
+import static data.scripts.util.US_utils.removeConditionIfNeeded;
 
 public class US_hyceanManager {
     public static final Map<String, Float> baseWeight = new HashMap<>();
     public static final List<String> groups = new ArrayList<>();
+    public static final List <String> REMOVED_CONDITIONS = new ArrayList<>();
 
     static {
-        //groups.add("atmosphere");
+        REMOVED_CONDITIONS.add(Conditions.HABITABLE);
+        REMOVED_CONDITIONS.add(Conditions.TECTONIC_ACTIVITY);
+        REMOVED_CONDITIONS.add(Conditions.EXTREME_TECTONIC_ACTIVITY);
+        REMOVED_CONDITIONS.add(Conditions.NO_ATMOSPHERE);
+        REMOVED_CONDITIONS.add(Conditions.THIN_ATMOSPHERE);
+        REMOVED_CONDITIONS.add(Conditions.TOXIC_ATMOSPHERE);
+        REMOVED_CONDITIONS.add(Conditions.DENSE_ATMOSPHERE);
+        REMOVED_CONDITIONS.add(Conditions.MILD_CLIMATE);
+        REMOVED_CONDITIONS.add(Conditions.EXTREME_WEATHER);
+        REMOVED_CONDITIONS.add(Conditions.INIMICAL_BIOSPHERE);
+        REMOVED_CONDITIONS.add(Conditions.METEOR_IMPACTS);
+        REMOVED_CONDITIONS.add(Conditions.POLLUTION);
+        REMOVED_CONDITIONS.add(Conditions.ORE_SPARSE);
+        REMOVED_CONDITIONS.add(Conditions.ORE_MODERATE);
+        REMOVED_CONDITIONS.add(Conditions.ORE_ABUNDANT);
+        REMOVED_CONDITIONS.add(Conditions.ORE_RICH);
+        REMOVED_CONDITIONS.add(Conditions.ORE_ULTRARICH);
+        REMOVED_CONDITIONS.add(Conditions.RARE_ORE_SPARSE);
+        REMOVED_CONDITIONS.add(Conditions.RARE_ORE_MODERATE);
+        REMOVED_CONDITIONS.add(Conditions.RARE_ORE_ABUNDANT);
+        REMOVED_CONDITIONS.add(Conditions.RARE_ORE_RICH);
+        REMOVED_CONDITIONS.add(Conditions.RARE_ORE_ULTRARICH);
+        REMOVED_CONDITIONS.add(Conditions.VOLATILES_TRACE);
+        REMOVED_CONDITIONS.add(Conditions.VOLATILES_DIFFUSE);
+        REMOVED_CONDITIONS.add(Conditions.VOLATILES_ABUNDANT);
+        REMOVED_CONDITIONS.add(Conditions.VOLATILES_PLENTIFUL);
+        REMOVED_CONDITIONS.add(Conditions.ORGANICS_TRACE);
+        REMOVED_CONDITIONS.add(Conditions.ORGANICS_COMMON);
+        REMOVED_CONDITIONS.add(Conditions.ORGANICS_ABUNDANT);
+        REMOVED_CONDITIONS.add(Conditions.ORGANICS_PLENTIFUL);
+        REMOVED_CONDITIONS.add(Conditions.FARMLAND_POOR);
+        REMOVED_CONDITIONS.add(Conditions.FARMLAND_ADEQUATE);
+        REMOVED_CONDITIONS.add(Conditions.FARMLAND_RICH);
+        REMOVED_CONDITIONS.add(Conditions.FARMLAND_BOUNTIFUL);
+        REMOVED_CONDITIONS.add("US_bedrock");
+        REMOVED_CONDITIONS.add("US_tunnels");
+        REMOVED_CONDITIONS.add("US_religious");
+        REMOVED_CONDITIONS.add("US_elevator");
+        REMOVED_CONDITIONS.add("US_floating");
+        REMOVED_CONDITIONS.add("US_crash");
+        REMOVED_CONDITIONS.add("US_base");
+
+        groups.add("atmosphere");
         groups.add("weather");
         groups.add("biosphere");
         groups.add("ore");
@@ -26,10 +70,9 @@ public class US_hyceanManager {
         groups.add("volatiles");
         groups.add("organics");
 
-        // TODO cat_frozen
+        // Atmosphere
         baseWeight.put("atmosphere_no_pick", 10f);
-        baseWeight.put(Conditions.THIN_ATMOSPHERE, 1f);
-        baseWeight.put(Conditions.DENSE_ATMOSPHERE, 1f);
+        baseWeight.put(Conditions.THIN_ATMOSPHERE, 2f);
 
         // Weather
         baseWeight.put("weather_no_pick", 10f);
@@ -41,16 +84,16 @@ public class US_hyceanManager {
 
         // Ores
         baseWeight.put("ore_no_pick", 100f);
-        baseWeight.put(Conditions.ORE_SPARSE, 30f);
-        baseWeight.put(Conditions.ORE_MODERATE, 30f);
+        baseWeight.put(Conditions.ORE_SPARSE, 15f);
+        baseWeight.put(Conditions.ORE_MODERATE, 20f);
         baseWeight.put(Conditions.ORE_ABUNDANT, 4f);
         baseWeight.put(Conditions.ORE_RICH, 1f);
 
         // Rare Ores
-        baseWeight.put("rare_ore_no_pick", 50f);
+        baseWeight.put("rare_ore_no_pick", 100f);
         baseWeight.put(Conditions.RARE_ORE_SPARSE, 10f);
-        baseWeight.put(Conditions.RARE_ORE_MODERATE, 5f);
-        baseWeight.put(Conditions.RARE_ORE_ABUNDANT, 5f);
+        baseWeight.put(Conditions.RARE_ORE_MODERATE, 15f);
+        baseWeight.put(Conditions.RARE_ORE_ABUNDANT, 4f);
         baseWeight.put(Conditions.RARE_ORE_RICH, 1f);
 
         // Volatiles
@@ -65,6 +108,10 @@ public class US_hyceanManager {
 
 
     public static void manageHyceanConditions(PlanetAPI planet) {
+        for (String condition : REMOVED_CONDITIONS){
+            removeConditionIfNeeded(planet, condition);
+        }
+
         addConditionIfNeeded(planet, Conditions.WATER_SURFACE);
 
         for (String group : groups) {
